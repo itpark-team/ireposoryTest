b67412488889ec047606af24fa14c71e5546f400
itpark <itpark.team@gmail.com>
2021-04-05 20:14:20 +0300

    first commit

---
+++ ConsoleAppIRepository/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppIRepository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
